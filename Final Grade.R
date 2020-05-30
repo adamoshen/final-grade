3146c59088718d952f602af741637408e1eb0694
tbonkers <46703463+tbonkers@users.noreply.github.com>
2020-05-30 00:45:38 -0400

    Add files via upload

---
+++ Final Grade.R
@@ -0,0 +1,57 @@
+# A basic script for calculating your required final exam grade in order to
+# obtain a B- or higher in a course based on your current progress. Allows
+# for percentage or fractional grade input.
+# For simplicity, no action has been taken to prevent invalid input from
+# the user, such as negative/textual grades, invalid weights, etc.
+
+library(stringr)
+
+# Collection step
+
+marks <- numeric(0)
+weights <- numeric(0)
+
+running <- TRUE
+
+while (running){
+  m <- readline("Enter mark as percentage or fraction (-1 to exit): ")
+  
+  if (m == "-1"){
+    running <- FALSE
+  } else {
+    w <- as.numeric(readline("Enter weight (%): "))
+    weights <- c(weights, w)
+    
+    if (!(str_detect(m, pattern="/"))){
+      ## Percentage grade entered
+      decimalGrade <- as.numeric(m)/100
+      marks <- c(marks, decimalGrade)
+    } else {
+      ## Fractional grade entered
+      frac <- as.numeric(str_split(m, pattern="/", n=2, simplify=TRUE))
+      decimalGrade <- frac[1]/frac[2]
+      marks <- c(marks, decimalGrade)
+    }
+  }
+}
+
+
+# Computation step
+
+examWeight <- 100 - sum(weights)
+points <- sum(marks*weights)
+
+## `gradeCut` can be modified to suit your needs
+gradeCut <- c(70, 75, 80, 85, 90)
+finalExamGrade <- (gradeCut - points)/(examWeight/100)
+## Reported final exam grades should not be negative
+finalExamGrade <- ifelse(finalExamGrade < 0, 0, finalExamGrade)
+
+
+# Output
+
+for (i in 1:length(gradeCut)){
+  cat("\n", sprintf("For a final grade of %d%% you need a %.2f%% on the final exam.",
+                    gradeCut[i], finalExamGrade[i]), sep="")
+}
+
