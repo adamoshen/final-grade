430093eba426370ff56cf0672745cd57caaac289
Adam Shen <46703463+adamshen1@users.noreply.github.com>
2020-11-25 23:48:05 -0500

    clean up
    Update exit condition, move stuff around for clarity, remove some spacing, use of `append` rather than `c`

--- final-grade.R
+++ final-grade.R
@@ -7,37 +7,35 @@
 library(stringr)
 
 # Collection step
-
 marks <- numeric(0)
 weights <- numeric(0)
 
 running <- TRUE
 
 while (running){
-  m <- readline("Enter mark as percentage or fraction (-1 to exit): ")
+  m <- readline("Enter mark as percentage or fraction (`e` to exit): ")
   
-  if (m == "-1"){
+  if (m == "e"){
     running <- FALSE
   } else {
     w <- as.numeric(readline("Enter weight (%): "))
-    weights <- c(weights, w)
+    weights <- append(weights, values=w)
     
-    if (!str_detect(m, pattern="/")){
-      ## Percentage grade entered
-      decimalGrade <- as.numeric(m)/100
-      marks <- c(marks, decimalGrade)
-    } else {
+    if (str_detect(m, pattern="/")){
       ## Fractional grade entered
       frac <- as.numeric(str_split(m, pattern="/", n=2, simplify=TRUE))
       decimalGrade <- frac[1]/frac[2]
-      marks <- c(marks, decimalGrade)
+      marks <- append(marks, values=decimalGrade)
+    } else {
+      ## Percentage grade entered
+      decimalGrade <- as.numeric(m)/100
+      marks <- append(marks, values=decimalGrade)
     }
   }
 }
 
 
 # Computation step
-
 examWeight <- 100 - sum(weights)
 points <- sum(marks*weights)
 
@@ -47,7 +45,6 @@ finalExamGrade <- (gradeCut - points)/(examWeight/100)
 
 
 # Output
-
 for (i in 1:length(gradeCut)){
   cat("\n", sprintf("For a final grade of %d%% you need a %.2f%% on the final exam.",
                     gradeCut[i], finalExamGrade[i]), sep="")
